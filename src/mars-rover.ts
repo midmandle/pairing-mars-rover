a1efc6f19da53d11df8ab68bbe8e69a1f0287144
Marius Bodnariuc <marius.bodnariuc@cazoo.co.uk>
2020-01-13 13:13:52 +0000

    Rover moves Left twice

--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -1,5 +1,11 @@
 export function execute(commands : string) {
-  if(commands === "L")
+  if(commands === "L") {
     return "0,0,W";
+  }
+
+  if(commands === "LL") {
+    return "0,0,S";
+  }
+
   return "0,0,N";
 }
